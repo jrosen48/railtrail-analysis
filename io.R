becbc7958656e7f951fb708755e65683d2a57372
Joshua Rosenberg <jrosen@msu.edu>
2017-08-13 20:35:56 -0400

    make major changes to functionals

--- io.R
+++ io.R
@@ -19,95 +19,98 @@ extract_numbers <- function(x) {
     x
 }
 
-# Main function to scrape data for a page
+scrape_one_url <- function(url) {
 
-scrape_data <- function(urls, n_to_scrape = NULL) {
+        page <- read_html(paste0("https://www.traillink.com/", url))
+        
+}
+
+process_page <- function(page) {
     
-    if (is.null(n_to_scrape)) n_to_scrape <- length(urls)
+    Sys.sleep(1)
     
     results_df <- tibble(
-        name =  vector(length = length(urls)),
-        distance = vector(length = length(urls)),
-        surface = vector(length = length(urls)),
-        category = vector(length = length(urls)),
-        mean_review = vector(length = length(urls)),
-        description = vector(length = length(urls)),
-        n_reviews = vector(length = length(urls))
+        name =  vector(length = 1),
+        distance = vector(length = 1),
+        surface = vector(length = 1),
+        category = vector(length = 1),
+        mean_review = vector(length = 1),
+        description = vector(length = 1),
+        n_reviews = vector(length = 1),
+        raw_reviews = vector(length = 1)
     )
+
+    page <- read_html(paste0("https://www.traillink.com/", page))
+ 
+    distance <- html_nodes(page, ".facts:nth-child(1) div:nth-child(3) span")
+    distance <- html_text(distance)
+    results_df$distance <- distance
     
-    out_list <- list()
+    name <- html_nodes(page, ".small-12 .section-header")
+    name <- html_text(name)
+    results_df$name <- name
     
-    for (i in 1:n_to_scrape) {
-        
-        print(paste0("Processing ", i, " out of ", n_to_scrape))
-        
-        page <- read_html(paste0("https://www.traillink.com/", urls[i]))
-        
-        distance <- html_nodes(page, ".facts:nth-child(1) div:nth-child(3) span")
-        distance <- html_text(distance)
-        results_df$distance[i] <- distance
-        
-        name <- html_nodes(page, ".small-12 .section-header")
-        name <- html_text(name)
-        results_df$name[i] <- name
-        
-        surface <- html_nodes(page, ".facts+ .facts div:nth-child(1) span")
-        surface <- html_text(surface)
-        results_df$surface[i] <- surface
-        
-        mean_review <- html_nodes(page, ".mid-up-test .fa-star")
-        mean_review <- html_text(mean_review)
-        results_df$mean_review[i] <- length(mean_review)
-        
-        category <- html_nodes(page, ".facts+ .facts div:nth-child(2) span")
-        category <- html_text(category)
-        results_df$category[i] <- category
-        
-        description <- html_nodes(page, ".wrap .medium-8")
-        description <- html_text(description)
-        results_df$description[i] <- description
-        
-        n_reviews <- html_nodes(page, ".reviews a")
-        n_reviews <- html_text(n_reviews)
-        results_df$n_reviews[i] <- n_reviews
-        
-        star <- html_nodes(page, "#trail-detail-reviews .medium-3 ")
-        
-        # Finding individual reviews
-        
-        out_vector <- vector(length = length(star))
-        
-        for (j in 1:length(star)) {
-            x <- html_nodes(star[j], ".star")
-            x <- length(html_text(x))
-            x[x == 0] <- NA
-            out_vector[j] <- x
-        }
-        
-        out_list[[i]] <- out_vector
-        
-        # results_df <- bind_cols(results_df, out_vector)
-        
-        Sys.sleep(1)
-        
+    surface <- html_nodes(page, ".facts+ .facts div:nth-child(1) span")
+    surface <- html_text(surface)
+    results_df$surface <- surface
+    
+    mean_review <- html_nodes(page, ".mid-up-test .fa-star")
+    mean_review <- html_text(mean_review)
+    results_df$mean_review <- length(mean_review)
+    
+    category <- html_nodes(page, ".facts+ .facts div:nth-child(2) span")
+    category <- html_text(category)
+    results_df$category <- category
+    
+    description <- html_nodes(page, ".wrap .medium-8")
+    description <- html_text(description)
+    results_df$description <- description
+    
+    n_reviews <- html_nodes(page, ".reviews a")
+    n_reviews <- html_text(n_reviews)
+    results_df$n_reviews <- n_reviews
+    
+    star <- html_nodes(page, "#trail-detail-reviews .medium-3 ")
+    
+    # Finding individual reviews
+    
+    out_vector <- vector(length = length(star))
+    
+    for (j in 1:length(star)) {
+        x <- html_nodes(star[j], ".star")
+        x <- length(html_text(x))
+        x[x == 0] <- NA
+        out_vector[j] <- x
     }
-    
-    tmp <- enframe(out_list)
-    
-    results_df <- results_df[1:n_to_scrape, ]
-    
-    results_df <- mutate(results_df,
-                         distance = extract_numbers(distance),
-                         n_reviews = extract_numbers(n_reviews),
-                         name = str_sub(name, end = -7L),
-                         raw_reviews = tmp$value
-                         )
-    
-    results_df
+
+    mutate(results_df,
+           raw_reviews = list(out_vector))
     
 }
 
-# Scraping
+process_all_pages <- function(.x = vector_of_pages, .y = state_names, .pb = NULL) {
+    
+    if ((!is.null(.pb)) && inherits(.pb, "Progress") && (.pb$i < .pb$n)) .pb$tick()$print()
+    
+    out <- map_df(vector_of_pages, possibly(process_page, NULL))
+    
+    write_rds(out, paste0("data/", state_names, ".rds"))
+    
+    return(out)
+    
+}
+
+find_urls <- function(x) {
+    
+    urls <- read_html(paste0("https://www.traillink.com", x)) %>%
+        html_nodes("a") %>% 
+        html_attr("href") %>% 
+        finding_trails()
+    
+    return(urls)
+}
+
+# Main analysis
 
 state_urls <- read_html("https://www.traillink.com/find-trails") %>%
     html_nodes("a") %>%
@@ -116,32 +119,14 @@ state_urls <- read_html("https://www.traillink.com/find-trails") %>%
 state_urls <- state_urls[str_sub(state_urls, start = 2, end = 6) == "state"]
 state_urls <- state_urls[!is.na(state_urls)]
 
-scrape_every_state <- function(state_urls, which_state = NULL, n_to_scrape = NULL) {
-    
-    if (!is.null(which_state)) {
-        state_urls <- state_urls[str_detect(state_urls, which_state)]
-    }
-    
-    for (i in 1:length(state_urls)){
-        
-        name <- state_urls %>% 
-            str_split("/") %>% 
-            map_chr( ~ .[3]) %>% 
-            str_split("-") %>% 
-            map_chr( ~ .[1])
-        
-        print(paste0("Processing data for ", name[i], " (", i, "/", length(name), ")"))
-        
-        urls <- read_html(paste0("https://www.traillink.com", state_urls[i])) %>%
-            html_nodes("a") %>% 
-            html_attr("href") %>% 
-            finding_trails()
-        
-        df <- scrape_data(urls, n_to_scrape)    
-        
-        write_rds(df, paste0("data/", name[i], ".rds"))
-        
-    }
-}
+name <- state_urls %>%
+    str_split("/") %>%
+    map_chr( ~ .[3]) %>%
+    str_split("-") %>%
+    map_chr( ~ .[1])
 
-scrape_every_state(state_urls, "mi")
+list_of_trails <- map(state_urls, find_urls)
+names(list_of_trails) <- name
+
+pb <- progress_estimated(length(list_of_trails))
+list_of_dfs <- map2_df(list_of_trails, name, process_all_pages, .id = "state", .pb = pb)
